0f78de0bbd55ffe6321bbcf1e8b1f36ad70fc265
Clark Perkins <r.clark.perkins@gmail.com>
2014-11-11 01:37:10 -0600

    herokuify the settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn sinfoniabot.wsgi --log-file -
--- sinfoniabot/settings.py
+++ sinfoniabot/settings.py
@@ -79,8 +79,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATIC_BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(STATIC_BASE_DIR, 'static'),
+)
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 BOT_ID = os.environ['BOT_ID']
 
